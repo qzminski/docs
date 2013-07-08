cfad854702aff1b79c97cad341ffce3d4d017971
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2013-07-08 15:33:40 +0200

    Further DCA parser improvements

--- playground/dca-docs/languages/en.php
+++ playground/dca-docs/languages/en.php
@@ -3,6 +3,7 @@
 /**
  * Config
  */
-$GLOBALS['TL_LANG']['tl_table']['config'] = 'Configuration';
-$GLOBALS['TL_LANG']['tl_table']['config.dataContainer'] = 'Data container driver';
-$GLOBALS['TL_LANG']['tl_table']['config.enableVersioning'] = 'True to enable versioning';
+$GLOBALS['TL_LANG']['tl_table']['config'][0] = 'Data container configuration';
+$GLOBALS['TL_LANG']['tl_table']['config'][1] = 'http://contao.org';
+$GLOBALS['TL_LANG']['tl_table']['config.dataContainer'][0] = 'Data container driver';
+$GLOBALS['TL_LANG']['tl_table']['config.enableVersioning'][1] = 'http://contao.org';
